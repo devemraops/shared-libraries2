d0dddf417051e57bfefd44175cefd676ba0eb07c
emrah basdemir <emrah.basdemir@icloud.com>
2023-08-12 14:40:46 -0500

    morefiles

---
+++ vars/checkoutScm.groovy
@@ -0,0 +1,5 @@
+def call() {
+    echo 'Checking out source code...'
+    checkout scm
+    echo 'Source code checked out successfully.'
+}
---
+++ vars/cleanupWorkspace.groovy
@@ -0,0 +1,5 @@
+def call() {
+    echo 'Cleaning up the workspace...'
+    cleanWs()
+    echo 'Workspace cleaned up successfully.'
+}
